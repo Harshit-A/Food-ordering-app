d1b2cc4a1b275010fea74b98a929553316277b4e
Harshit <harshit26496@gmail.com>
2021-03-25 21:52:43 +0530

    crud apis for admin

--- food_ordering_server/controllers/store.controller.js
+++ food_ordering_server/controllers/store.controller.js
@@ -1,6 +1,8 @@
 const db = require('../db');
 const Store = db.store;
 const Item = db.item;
+const StoreItem = db.storeItem;
+const operators = db.Sequelize.Op;
 
 exports.listStores = (req,res)=>{
 	Store.findAll({}).then(stores =>{
@@ -58,3 +60,85 @@ exports.addItemsToStore = (req,res) =>{
 	});
 };
 
+exports.updateItemAvailability =  (req,res) =>{
+	itemsWithAvailabilityN = [];
+	if(req.body.itemsWithAvailabilityN)
+		itemsWithAvailabilityN = req.body.itemsWithAvailabilityN.split(',');
+	itemsWithAvailabilityY = [];
+	if(req.body.itemsWithAvailabilityY)
+	 	itemsWithAvailabilityY = req.body.itemsWithAvailabilityY.split(',');
+ 
+	StoreItem.update(
+		{ is_available : 'Y'},
+		{ where : {
+			storeId  : req.params.storeId,
+			itemId : itemsWithAvailabilityY
+	}})
+	.catch(err =>{
+		res.status(500).send({message : err.message});
+	}).then(()=>{
+		StoreItem.update(
+		{ is_available : 'N'},
+		{ where : {
+			storeId  : req.params.storeId,
+			itemId : itemsWithAvailabilityN
+		}})
+		.catch(err =>{
+			res.status(500).send({message : err.message});
+		});	
+	}).then(()=>{
+		res.status(200).send({message : 'Items have been updated'});
+	});	
+};
+
+exports.deleteItemFromStore = (req,res) =>{
+	const itemIds = req.body.itemId.split(',');
+	StoreItem.destroy({
+		where: {
+			storeId : req.params.storeId,
+			itemId : itemIds
+		}
+	}).catch(err=>{
+		res.status(500).send({message : err.msg});
+	}).then(()=>{
+		res.status(200).send({message : 'Items have been deleted'});
+	});
+};
+
+exports.deleteItems = (req,res) =>{
+	const itemIds = req.body.itemId.split(',');
+	StoreItem.destroy({
+		where :{
+			itemId : itemIds
+		}
+	}).then(() =>{
+		Item.destroy({
+			where : {
+				id : itemIds
+			}
+		})
+	}).then(() =>{
+		res.status(200).send({message: 'Items have been deleted from all stores'});
+	}).catch(err=>{
+		res.status(500).send({message : err.message});
+	});
+};
+
+exports.deleteStores = (req,res) =>{
+	const storeIds = req.body.storeId.split(',');
+	StoreItem.destroy({
+		where : {
+			storeId : storeIds
+		}
+	}).then(() =>{
+		Store.destroy({
+			where : {
+				id : storeIds
+			}
+		})
+	}).then(() =>{
+		res.status(200).send({message : 'Stores have been deleted'});
+	}).catch(err =>{
+		res.status(500).send({message : err.msg});
+	});
+};
--- food_ordering_server/routes/admin.routes.js
+++ food_ordering_server/routes/admin.routes.js
@@ -5,5 +5,10 @@ const storeController = require('../controllers/store.controller');
 //TO_DO: add middleware to control access
 router.get('/items', storeController.getItems);
 router.post('/addItems/:storeId', storeController.addItemsToStore)
+router.post('/:storeId/updateItem', storeController.updateItemAvailability);
+router.post('/:storeId/deleteItem', storeController.deleteItemFromStore);
+router.post('/deleteItem', storeController.deleteItems);
+router.post('/deleteStore', storeController.deleteStores);
+
 module.exports = router;
 
